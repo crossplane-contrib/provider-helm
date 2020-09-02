eabdc9ebb3d1ee0845affaf6a173352f0db513f1
Hasan Turken <turkenh@gmail.com>
2020-09-02 17:13:25 +0300

    Consume kubeconfig key if found
    Fixes #18
    
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- pkg/clients/client.go
+++ pkg/clients/client.go
@@ -17,16 +17,29 @@ limitations under the License.
 package clients
 
 import (
+	"fmt"
 	"net/url"
 
 	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
 	"github.com/pkg/errors"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // NewRestConfig returns a rest config given a secret with connection information.
 func NewRestConfig(creds map[string][]byte) (*rest.Config, error) {
+	// If "kubeconfig" key found, use it
+	kc, f := creds[runtimev1alpha1.ResourceCredentialsSecretKubeconfigKey]
+	if f {
+		ac, err := clientcmd.Load(kc)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to load kubeconfig")
+		}
+		return restConfigFromAPIConfig(ac)
+	}
+
 	u, err := url.Parse(string(creds[runtimev1alpha1.ResourceCredentialsSecretEndpointKey]))
 	if err != nil {
 		return nil, errors.Wrapf(err, "cannot parse Kubernetes endpoint as URL")
@@ -58,4 +71,40 @@ func NewKubeClient(config *rest.Config) (client.Client, error) {
 	}
 
 	return kc, nil
+}
+
+func restConfigFromAPIConfig(c *api.Config) (*rest.Config, error) {
+	ctx := c.CurrentContext
+	if ctx == "" {
+		return nil, errors.New("currentContext not set in kubeconfig")
+	}
+	cluster := c.Clusters[ctx]
+	if cluster == nil {
+		return nil, errors.New(fmt.Sprintf("cluster for currentContext (%s) not found", ctx))
+	}
+	user := c.AuthInfos[ctx]
+	if user == nil {
+		return nil, errors.New(fmt.Sprintf("auth info for currentContext (%s) not found", ctx))
+	}
+	return &rest.Config{
+		Host:            cluster.Server,
+		Username:        user.Username,
+		Password:        user.Password,
+		BearerToken:     user.Token,
+		BearerTokenFile: user.TokenFile,
+		Impersonate: rest.ImpersonationConfig{
+			UserName: user.Impersonate,
+			Groups:   user.ImpersonateGroups,
+			Extra:    user.ImpersonateUserExtra,
+		},
+		AuthProvider: user.AuthProvider,
+		ExecProvider: user.Exec,
+		TLSClientConfig: rest.TLSClientConfig{
+			Insecure:   cluster.InsecureSkipTLSVerify,
+			ServerName: cluster.TLSServerName,
+			CertData:   user.ClientCertificateData,
+			KeyData:    user.ClientKeyData,
+			CAData:     cluster.CertificateAuthorityData,
+		},
+	}, nil
 }
