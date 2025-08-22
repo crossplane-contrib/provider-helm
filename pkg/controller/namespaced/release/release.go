144f2db0db914aee25f011ec643cd15724e64b5f
Ty Christensen <tychristensen97@gmail.com>
2025-08-22 16:05:14 -0700

    make the default more clear
    Signed-off-by: Ty Christensen <tychristensen97@gmail.com>

--- pkg/controller/namespaced/release/release.go
+++ pkg/controller/namespaced/release/release.go
@@ -144,10 +144,11 @@ type connector struct {
 
 func withRelease(cr *v1beta1.Release) helmClient.ArgsApplier {
 	return func(config *helmClient.Args) {
-		// Use the namespace specified in the Managed Release spec, or default to the Managed Release's own namespace.
-		config.Namespace = cr.Spec.ForProvider.Namespace
-		if config.Namespace == "" {
-			config.Namespace = cr.Namespace
+		// Default to the Managed Release's own namespace.
+		config.Namespace = cr.Namespace
+		if cr.Spec.ForProvider.Namespace != "" {
+			// Use the namespace specified in the Managed Release spec, if set.
+			config.Namespace = cr.Spec.ForProvider.Namespace
 		}
 
 		config.Wait = cr.Spec.ForProvider.Wait
